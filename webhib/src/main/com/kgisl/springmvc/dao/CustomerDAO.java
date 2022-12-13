fa9ebde69822978cc39b3f4dbc619e71fc087a25
Praveen. p <85276447+Praveenp-maker@users.noreply.github.com>
2022-12-13 15:53:04 +0530

    springmvc+hibernate

---
+++ webhib/src/main/com/kgisl/springmvc/dao/CustomerDAO.java
@@ -0,0 +1,15 @@
+package com.kgisl.springmvc.dao;
+import java.util.List;
+
+import net.javaguides.springmvc.entity.Customer;
+
+public interface CustomerDAO {
+
+    public List < Customer > getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+}
